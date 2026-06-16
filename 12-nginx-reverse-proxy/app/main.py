60c64d2bad070c03b599fc03a290382822411dda
nora <nora.givi@gmail.com>
2026-06-16 18:28:40 +0330

    reverse proxy

---
+++ 12-nginx-reverse-proxy/app/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Hello from backend app"})
+
+@app.route("/api/status")
+def status():
+    return jsonify({"status": "running behind nginx"})
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
